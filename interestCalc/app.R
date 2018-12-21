20dc52f2842a83a82b2805575161613e16450450
Matt <mdseege@nppd.com>
2018-12-21 08:25:55 -0600

    sup?

--- interestCalc/app.R
+++ interestCalc/app.R
@@ -12,24 +12,29 @@ library(shiny)
 # Define UI for application that calculates interest
 ui <- fluidPage(
   # Application title
-  titlePanel("Interest Calculator"),
-  # Sidebar with a slider input for principle, interenst, and years 
-  sliderInput('principle', 'Principle', min = 0, max = 10000, value = 0, sep = ',', pre = '$', step = 500),
-  sliderInput('interest', 'Interest Rate Per Year', min = 0, max = 20, value = 0, post = '%', step = .1),
-  sliderInput('years','Years', min = 1, max = 100, value = 1, step = 1),
-  
-  # Show the interest earned with the preceeding input values
-  mainPanel(
-    h3(textOutput('text1')),
-    h3('Interest Earned')
+  titlePanel(h1('Interest Calculator')),
+  sidebarLayout(
+    sidebarPanel(
+      # Sidebar with a slider input for principle, interenst, and years 
+      sliderInput('principle', 'Principle', min = 0, max = 5000, value = 0, sep = ',', pre = '$', step = 500),
+      sliderInput('interest', 'Interest Rate Per Year', min = 0, max = 10, value = 0, post = '%', step = .1),
+      sliderInput('years','Years', min = 1, max = 50, value = 1, step = 1)),
+    # Show the interest earned with the preceeding input values
+    mainPanel(
+      h2('Move sliders to choose Principle, Interest Rate Per Year, and Years.'),
+      h2('With the parameters chosen above, interest will be calculated and displayed below.'),
+      h2('Interest Earned:'),
+      h2(textOutput('text1')
+         )
+      )
+    )
   )
-)
 
 # Define server logic required to calculate interest earned
 server <- function(input, output) {
   output$text1 <- renderText({
     # calculate interest earned from input$principle, input$interest, and inpupt$years from ui.R
-    paste('$',round(input$principle*exp((input$interest/100)*input$years)-input$principle,2))
+    paste('$',round(input$principle*exp((input$interest/100)*input$years)-input$principle,0))
   })
 }
 
